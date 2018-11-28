b343b07ea450454cd75e641116720c35f1d266c8
Junaid Siddiqui <junaid.wim@gmail.com>
2018-11-28 14:16:58 -0500

    installed google maps react component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import GoogleMapReact from 'google-map-react';
 
 import Footer from "../components/footer.js"
 import Header from "../components/header.js"
@@ -15,6 +16,18 @@ const IndexPage = () => (
       <p id="homepage-description-content">Founded in 2017, Signature Organic is a family run business focused on bringing you the best organic produce in the twin cities. We deliver free range poultry, fresh organic eggs, and vegetables and salads. Order now to get farm fresh produce at your doorstep!</p>
     </div>
     <ReviewSection></ReviewSection>
+    <div id="homepage-google-map">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyC_MHkASVBo7Iq95eQdPxXROl-uZ70HYKE"}}
+         center={{
+          lat: 26.092891,
+          lng: 85.948970
+        }}
+         zoom ={5}
+         >
+       </GoogleMapReact>
+
+    </div>
     <Footer className="is-col">
     </Footer>
   </div>
