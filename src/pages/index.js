3d506c9b8a3cba6c8748c464f2de39f6de4fd8aa
Junaid Siddiqui <junaid.wim@gmail.com>
2018-11-28 15:00:31 -0500

    minor stylistic changes, and addition of React Helmet to use Google Maps React

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import GoogleMapReact from 'google-map-react';
+import {Helmet} from "react-helmet";
 
 import Footer from "../components/footer.js"
 import Header from "../components/header.js"
@@ -7,12 +8,18 @@ import ReviewSection from "../components/ReviewSection.js"
 
 import '../styles/masterstyles.scss'
 
+
+
 const IndexPage = () => (
   <div id="homepage-body">
+    <Helmet>
+      <script src="https://maps.googleapis.com/maps/api/js" async defer></script>
+    </Helmet>
     <div id="homepage-hero" className="is-row">
       <Header></Header>
     </div>
-    <div id="homepage-company-description" className="is-col">
+    <div id="homepage-company-description" className="is-col is-shadow-1">
+      <h3 className="section-headers">Let us feed you!</h3>
       <p id="homepage-description-content">Founded in 2017, Signature Organic is a family run business focused on bringing you the best organic produce in the twin cities. We deliver free range poultry, fresh organic eggs, and vegetables and salads. Order now to get farm fresh produce at your doorstep!</p>
     </div>
     <ReviewSection></ReviewSection>
